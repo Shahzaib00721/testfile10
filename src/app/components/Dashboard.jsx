47c02a599e15ee914b42b870002b9535df9d35c3
Shahzaib Awan <awan.shahzaib.0314@gmail.com>
2025-10-02 15:00:12 +0500

    update mae

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,30 +1,32 @@
- "use client";
- export default function Dashboard() {
+"use client";
+export default function Dashboard() {
   return (
-    <div className=" shadow rounded-lg h-100   w-70  ">
-      <h2 className="text-lg  mb-6 text-black leading-[20px] pl-10 mt-10 ">Hello, Alberta</h2>
-      <p className="text-gray-600 text-md mb-5  leading-[20px] pl-10 ">
-        Nice work! Let’s keep going.<br />
-        <br/> 
-        
-        One down, two to go!
-        <br/>
-        <br/>
-        
+    <div className="flex items-center justify-center  ">
+      {/* outer container center کرے گا */}
 
-      </p>
-      <ul className="list-disc list-inside 
- text-sm space-y-1">
-        <li className="mb-4 pl-10">Add your first &lt;code&gt;</li>
-        <li className="mb-4 text-[rgb(40,159,192,1)] pl-10">Create a program</li>
-        <li className=" text-[rgb(40,159,192,1)] pl-10">Add a staff member</li>
-      </ul>
-     
+      <div className="bg-gray-50 shadow rounded-lg h-auto w-full max-w-md md:h-100 ">
+        <h2 className="text-lg mb-6 text-black leading-[20px] pl-4 mt-6">
+          Hello, Alberta
+        </h2>
+        <p className="text-gray-600 text-md mb-5 leading-[20px] pl-4">
+          Nice work! Let’s keep going.<br />
+          <br />
+          One down, two to go!
+          <br />
+          <br />
+        </p>
+        <ul className="list-disc list-inside text-sm space-y-1 pl-4">
+          <li className="mb-4">Add your first &lt;code&gt;</li>
+          <li className="mb-4 text-[rgb(40,159,192,1)]">Create a program</li>
+          <li className="text-[rgb(40,159,192,1)]">Add a staff member</li>
+        </ul>
 
-  <img src="imgg.png" alt="" className=" bg-fixed bg-cover bg-center  " />
-
-
+        <img
+          src="/imgg.png"
+          alt=""
+          className="bg-fixed bg-cover bg-center w-full rounded-b-lg"
+        />
+      </div>
     </div>
-    
   );
 }
